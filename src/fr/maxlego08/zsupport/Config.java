2d8bc00aeba81aaca44cfb66d0547ccebab1e366
Maxlego08 <maxlego08@sfr.fr>
2022-02-11 20:31:12 +0100

    :bug: Fix emote

--- src/fr/maxlego08/zsupport/Config.java
+++ src/fr/maxlego08/zsupport/Config.java
@@ -29,7 +29,7 @@ public class Config implements Saveable {
 
 	static {
 
-		plugins.add(new Plugin("zAuctionHouse", 712306983251738625l, 613374375240138761l, 1, 10));
+		plugins.add(new Plugin("zAuctionHouse", 809473314077802496l, 613374375240138761l, 1, 10));
 		plugins.add(new Plugin("zShop", 712307096607129611l, 666552224490455041l, 2, 8));
 		plugins.add(new Plugin("zSpawner", 712306863869263942l, 613374745186009097l, 4, 6));
 		plugins.add(new Plugin("zNexus", 712307057092460584l, 700417597996400680l, 5, 6));
@@ -39,7 +39,7 @@ public class Config implements Saveable {
 		plugins.add(new Plugin("zTournament", 731802368152174622l, 730685641972645950l, 3, 15));
 		plugins.add(new Plugin("zFactionRanking", 731802322211962881l, 729365118609129472l, 7, 10));
 		plugins.add(new Plugin("zMobFighter", 843564378946273350l, 843564877611794482l, 41, 8));
-		plugins.add(new Plugin("zAuctionHouse Redis", 941374457451249716l, 941723181348192277l, 210, 15));
+		plugins.add(new Plugin("zAuctionHouse Redis", 941723116873326623l, 941723181348192277l, 210, 15));
 		plugins.add(new Plugin("zVoteParty", 879471509930926121l, 0l, 124, 0));
 		plugins.add(new Plugin("zOldEnchant", 941724257132634152l, 0l, 221, 0));
 
