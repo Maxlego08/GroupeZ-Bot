4ffc0a6e95e93a9a230883de7f20c2bdc355e917
Maxlego08 <maxlego08@sfr.fr>
2024-02-02 22:21:57 +0100

    :memo: Comment

--- src/fr/maxlego08/zsupport/faq/FaqManager.java
+++ src/fr/maxlego08/zsupport/faq/FaqManager.java
@@ -18,23 +18,52 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Manages FAQs for the support system, including retrieval, creation, and deletion of FAQs.
+ */
 public class FaqManager extends ZUtils {
 
+    /**
+     * The SQL manager to interact with the database.
+     */
     private final SqlManager sqlManager;
+
+    /**
+     * A list of FAQs currently loaded.
+     */
     private List<Faq> faqs = new ArrayList<>();
 
+    /**
+     * Constructs a new FAQ Manager.
+     *
+     * @param sqlManager The SQL manager to use for database interactions.
+     */
     public FaqManager(SqlManager sqlManager) {
         this.sqlManager = sqlManager;
     }
 
+    /**
+     * Retrieves an FAQ by its name.
+     *
+     * @param name The name of the FAQ to retrieve.
+     * @return An optional containing the FAQ if found, or empty otherwise.
+     */
     private Optional<Faq> get(String name) {
         return this.faqs.stream().filter(faq -> faq.getName().equals(name)).findFirst();
     }
 
+    /**
+     * Generates a list of command choices for FAQs.
+     *
+     * @return A list of command choices for use in commands.
+     */
     public List<CommandChoice> getChoices() {
         return this.faqs.stream().map(faq -> new CommandChoice(faq.getTitle(), faq.getName())).collect(Collectors.toList());
     }
 
+    /**
+     * Loads FAQs from the database and registers FAQ-related commands.
+     */
     public void selectFqa() {
         SqlManager.service.execute(() -> {
             this.faqs = this.sqlManager.getAllFaqs();
@@ -45,6 +74,12 @@ public class FaqManager extends ZUtils {
         });
     }
 
+    /**
+     * Sends an FAQ in response to a slash command interaction.
+     *
+     * @param event The event of the slash command interaction.
+     * @param name  The name of the FAQ to send.
+     */
     public void sendFaq(SlashCommandInteractionEvent event, String name) {
         Optional<Faq> optional = get(name);
         if (optional.isEmpty()) {
@@ -61,6 +96,14 @@ public class FaqManager extends ZUtils {
         event.replyEmbeds(builder.build()).queue();
     }
 
+    /**
+     * Creates a new FAQ and adds it to the database.
+     *
+     * @param event       The event of the slash command interaction.
+     * @param name        The name of the new FAQ.
+     * @param title       The title of the new FAQ.
+     * @param description The description (answer) of the new FAQ.
+     */
     public void createFaq(SlashCommandInteractionEvent event, String name, String title, String description) {
 
         if (get(name).isPresent()) {
@@ -75,6 +118,12 @@ public class FaqManager extends ZUtils {
         event.reply(":white_check_mark: You just create the FAQ:" + name).setEphemeral(true).queue();
     }
 
+    /**
+     * Deletes an FAQ by its name.
+     *
+     * @param event The event of the slash command interaction.
+     * @param name  The name of the FAQ to delete.
+     */
     public void deleteFaq(SlashCommandInteractionEvent event, String name) {
 
         Optional<Faq> optional = get(name);
